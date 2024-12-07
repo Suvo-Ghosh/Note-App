3c4cacb620d3af869338632b18fae6b283e10669
Suvo Ghosh <suvosh663@gmail.com>
2024-12-08 00:54:17 +0530

    Add tooltips

--- src/component/Paste.jsx
+++ src/component/Paste.jsx
@@ -70,46 +70,86 @@ function Paste() {
                                 <div className="flex flex-col sm:flex-row justify-between">
                                     <h1 className="text-2xl sm:text-3xl">{paste.title.slice(0, 40)}</h1>
                                     <div className="flex flex-row gap-2 mr-3 mt-1">
-                                        <button className="h-8 p-1 border-2 rounded-sm hover:text-yellow-500  duration-300">
-                                            <NavLink to={`/?pasteId=${paste?._id}`} aria-label="Edit Paste">
-                                                <FaEdit size={20} />
-                                            </NavLink>
-                                        </button>
-                                        <button className="p-1 h-8 border-2 rounded-sm hover:text-blue-400  duration-300">
-                                            <NavLink to={`/paste/${paste?._id}`} aria-label="View Paste">
-                                                <FaEye size={20} />
-                                            </NavLink>
-                                        </button>
-                                        <button
-                                            className="p-1 h-8 border-2 rounded-sm hover:text-red-700  duration-300"
-                                            onClick={() => handleDelete(paste?._id)}
-                                            aria-label="Delete Paste"
-                                        >
-                                            <FaTrashAlt size={20} />
-                                        </button>
-                                        <button
-                                            className="p-1 h-8 border-2 rounded-sm hover:text-green-700  duration-300"
-                                            onClick={() => {
-                                                navigator.clipboard.writeText(paste?.content)
-                                                toast.success("Copied to clipboard", {
-                                                    position: 'top-right'  // This places the toast at the top-right corner
-                                                })
-                                            }}
-                                            aria-label="Copy Paste Content"
-                                        >
-                                            <FaCopy size={20} />
-                                        </button>
-                                        <button className="p-1 h-8 border-2 rounded-sm hover:text-blue-700  duration-300"
-                                            onClick={() => handleShare(paste.content, paste.title)}
-                                            aria-label="Share Paste">
-                                            <FaShareAlt size={20} />
-                                        </button>
+                                        <div className="relative">
+                                            <button className="h-8 p-1 border-2 rounded-sm hover:text-yellow-500  duration-300">
+                                                <NavLink
+                                                    to={`/?pasteId=${paste?._id}`}
+                                                    aria-label="Edit Paste"
+                                                    className="peer"
+                                                >
+                                                    <FaEdit size={20} />
+                                                </NavLink>
+                                                <div className="absolute bottom-full mb-2 left-1/2 transform -translate-x-1/2 px-2 py-1 bg-gray-700 text-white text-sm rounded-sm  transition-opacity duration-500 hidden peer-hover:flex">
+                                                    Edit
+                                                    <div className="absolute left-1/2 -bottom-2 transform -translate-x-1/2 w-0 h-0 border-l-[5px] border-r-[5px] border-t-[10px] border-transparent border-t-gray-700"></div>
+                                                </div>
+                                            </button>
+                                        </div>
+                                        <div className="relative">
+                                            <button className="p-1 h-8 border-2 rounded-sm hover:text-blue-400  duration-300">
+                                                <NavLink
+                                                    to={`/paste/${paste?._id}`}
+                                                    aria-label="View Paste"
+                                                    className="peer"
+                                                >
+                                                    <FaEye size={20} />
+                                                </NavLink>
+                                                <div className="absolute bottom-full mb-2 left-1/2 transform -translate-x-1/2 px-2 py-1 bg-gray-700 text-white text-sm rounded-sm  transition-opacity duration-500 hidden peer-hover:flex">
+                                                    View
+                                                    <div className="absolute left-1/2 -bottom-2 transform -translate-x-1/2 w-0 h-0 border-l-[5px] border-r-[5px] border-t-[10px] border-transparent border-t-gray-700"></div>
+                                                </div>
+                                            </button>
+                                        </div>
+                                        <div className="relative">
+                                            <button
+                                                className="p-1 h-8 border-2 rounded-sm hover:text-red-700  duration-300"
+                                                onClick={() => handleDelete(paste?._id)}
+                                                aria-label="Delete Paste"
+
+                                            >
+                                                <FaTrashAlt size={20} className="peer" />
+                                                <div className="absolute bottom-full mb-2 left-1/2 transform -translate-x-1/2 px-2 py-1 bg-gray-700 text-white text-sm rounded-sm  transition-opacity duration-500 hidden peer-hover:flex">
+                                                    Delete
+                                                    <div className="absolute left-1/2 -bottom-2 transform -translate-x-1/2 w-0 h-0 border-l-[5px] border-r-[5px] border-t-[10px] border-transparent border-t-gray-700"></div>
+                                                </div>
+                                            </button>
+
+                                        </div>
+                                        <div className="relative">
+                                            <button
+                                                className="p-1 h-8 border-2 rounded-sm hover:text-green-700  duration-300"
+                                                onClick={() => {
+                                                    navigator.clipboard.writeText(paste?.content)
+                                                    toast.success("Copied to clipboard", {
+                                                        position: 'top-right'  // This places the toast at the top-right corner
+                                                    })
+                                                }}
+                                                aria-label="Copy Paste Content"
+                                            >
+                                                <FaCopy size={20} className="peer" />
+                                                <div className="absolute bottom-full mb-2 left-1/2 transform -translate-x-1/2 px-2 py-1 bg-gray-700 text-white text-sm rounded-sm  transition-opacity duration-500 hidden peer-hover:flex">
+                                                    Copy
+                                                    <div className="absolute left-1/2 -bottom-2 transform -translate-x-1/2 w-0 h-0 border-l-[5px] border-r-[5px] border-t-[10px] border-transparent border-t-gray-700"></div>
+                                                </div>
+                                            </button>
+                                        </div>
+                                        <div className="relative">
+                                            <button className="p-1 h-8 border-2 rounded-sm hover:text-blue-700  duration-300"
+                                                onClick={() => handleShare(paste.content, paste.title)}
+                                                aria-label="Share Paste">
+                                                <FaShareAlt size={20} className="peer" />
+                                                <div className="absolute bottom-full mb-2 left-1/2 transform -translate-x-1/2 px-2 py-1 bg-gray-700 text-white text-sm rounded-sm  transition-opacity duration-500 hidden peer-hover:flex">
+                                                    Share
+                                                    <div className="absolute left-1/2 -bottom-2 transform -translate-x-1/2 w-0 h-0 border-l-[5px] border-r-[5px] border-t-[10px] border-transparent border-t-gray-700"></div>
+                                                </div>
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                                 <div className="flex flex-col-reverse sm:flex-row justify-between mt-2">
                                     <div className='w-full sm:w-[70%] '>{paste.content.slice(0, 200)}</div>
                                     <div className='flex '>
-                                        <div className="mt-1"><FaCalendar size={17} color="blue" className='mr-1' /></div>
+                                        <div className="mt-1"><FaCalendar size={17} className='mr-1' /></div>
                                         <div className="">{formatDate(paste.createAt)}</div>
                                     </div>
                                 </div>
--- src/component/ViewPaste.jsx
+++ src/component/ViewPaste.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { FaCopy } from 'react-icons/fa'
+import toast from 'react-hot-toast'
 
 function ViewPaste() {
     const { id } = useParams()
@@ -23,14 +24,20 @@ function ViewPaste() {
 
                 </div>
                 <div className="mt-4 rounded  w-full min-w-[300px] border-2">
-                    <div className="h-9 border-b-2 flex flex-row justify-between">
+                    <div className="h-[7%] border-b-2 border-b-gray-300 flex flex-row justify-between">
                         <div className=" p-2 flex flex-row gap-2">
                             <div className="h-4 w-4 bg-red-500 rounded-full"></div>
                             <div className="h-4 w-4 bg-yellow-400 rounded-full"></div>
                             <div className="h-4 w-4 bg-green-400 rounded-full"></div>
                         </div>
-                        <div className="p-2">
-                            <FaCopy size={20} />
+                        <div
+                            className="p-2 cursor-pointer"
+                            onClick={() => {
+                                navigator.clipboard.writeText(paste.content)
+                                toast.success("Copied to clipboard")
+                            }}
+                        >
+                            <FaCopy size={20} color='white' />
                         </div>
                     </div>
                     <textarea
